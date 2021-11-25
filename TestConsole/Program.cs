7999a59706aecb53e951a45d3bdb7e867e570371
deanxx <deanxx999@gmail.com>
2021-11-25 18:15:51 +0900

    project function test

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Adams.RepositoryService.Client;
+using Adams.RepositoryService.Models;
+using NAVIAIServices.RepositoryService.Enums;
+using System;
 
 namespace TestConsole
 {
@@ -6,7 +9,17 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            //var client = Adams.RepositoryService.Client.AdamsClientFactory.Create("localho");
+            var adamsClient = AdamsClientFactory.Create("http://localhost:5005");
+            var loginResult = adamsClient.LoginAsync("u1", "123").Result;
+            var createproject = new CreateProject("pppp1", "descdesc", "Mercury");
+            var createprojectresult = adamsClient.CreateProjectAsync(createproject).Result;
+            var createproject2 = new CreateProject("pppp2", "descdesc", "Mercury");
+            var createprojectresult2 = adamsClient.CreateProjectAsync(createproject).Result;
+            var getprojectallresult = adamsClient.GetAllProjectAsync().Result;
+            var getprojectresult = adamsClient.GetProjectAsync(createprojectresult.Id).Result;
+            var deleteprojectresult = adamsClient.DeleteProjectAsync(createprojectresult.Id).Result; // 아직 구현안함
+
+
         }
     }
 }
