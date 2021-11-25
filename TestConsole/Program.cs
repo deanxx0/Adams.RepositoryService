11a1659ff75fc48177f46ce0dc3372ae5be1ad23
deanxx <deanxx999@gmail.com>
2021-11-25 18:44:39 +0900

    test class info

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -11,13 +11,23 @@ namespace TestConsole
         {
             var adamsClient = AdamsClientFactory.Create("http://localhost:5005");
             var loginResult = adamsClient.LoginAsync("u1", "123").Result;
-            var createproject = new CreateProject("pppp1", "descdesc", "Mercury");
-            var createprojectresult = adamsClient.CreateProjectAsync(createproject).Result;
-            var createproject2 = new CreateProject("pppp2", "descdesc", "Mercury");
-            var createprojectresult2 = adamsClient.CreateProjectAsync(createproject).Result;
-            var getprojectallresult = adamsClient.GetAllProjectAsync().Result;
-            var getprojectresult = adamsClient.GetProjectAsync(createprojectresult.Id).Result;
-            var deleteprojectresult = adamsClient.DeleteProjectAsync(createprojectresult.Id).Result; // 아직 구현안함
+            var projectClient = adamsClient.CreateProjectClient("e020ed9f-0b26-4264-b63a-d26764a42ccd");
+
+            // project
+            //var createproject = new CreateProject("pppp1", "descdesc", "Mercury");
+            //var createprojectresult = adamsClient.CreateProjectAsync(createproject).Result;
+            //var createproject2 = new CreateProject("pppp2", "descdesc", "Mercury");
+            //var createprojectresult2 = adamsClient.CreateProjectAsync(createproject).Result;
+            //var getprojectallresult = adamsClient.GetAllProjectAsync().Result;
+            //var getprojectresult = adamsClient.GetProjectAsync(createprojectresult.Id).Result;
+            //var deleteprojectresult = adamsClient.DeleteProjectAsync(createprojectresult.Id).Result; // 아직 구현안함
+
+            // class info
+            //var createClassInfo = new CreateClassInfo("testClassInfo2", "dedede", 0, 0, 0);
+            //var createClassInfoResult = projectClient.ClassInfos.Create(createClassInfo);
+            //var AllClassInfo = projectClient.ClassInfos.GetAll();
+            //var classInfo = projectClient.ClassInfos.Get(createClassInfoResult.Id);
+            //var deleteClassInfo = projectClient.ClassInfos.Delete("2fc9334a-bcce-495e-be66-cd62e3e51e37");
 
 
         }
