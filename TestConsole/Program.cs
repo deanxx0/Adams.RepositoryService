bd4cb62577a7ec6beccb711ca759d2bf62eb1c40
deanxx <deanxx999@gmail.com>
2021-11-26 11:23:43 +0900

    test config and augmentation

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -40,14 +40,112 @@ namespace TestConsole
             //var getallRes2 = projectClient.InputChannels.GetAll();
 
             // meta key
-            var createModel = new CreateMetadataKey("test1", "dfaf", "string");
-            var createRes = projectClient.MetadataKeys.Create(createModel);
-            var createModel2 = new CreateMetadataKey("test2", "dfaf", "boolean");
-            var createRes2 = projectClient.MetadataKeys.Create(createModel2);
-            var getallRes = projectClient.MetadataKeys.GetAll();
-            var getRes = projectClient.MetadataKeys.Get(createRes2.Id);
-            var delRes = projectClient.MetadataKeys.Delete(createRes2.Id);
-            var getallRes2 = projectClient.MetadataKeys.GetAll();
+            //var createModel = new CreateMetadataKey("test1", "dfaf", "string");
+            //var createRes = projectClient.MetadataKeys.Create(createModel);
+            //var createModel2 = new CreateMetadataKey("test2", "dfaf", "boolean");
+            //var createRes2 = projectClient.MetadataKeys.Create(createModel2);
+            //var getallRes = projectClient.MetadataKeys.GetAll();
+            //var getRes = projectClient.MetadataKeys.Get(createRes2.Id);
+            //var delRes = projectClient.MetadataKeys.Delete(createRes2.Id);
+            //var getallRes2 = projectClient.MetadataKeys.GetAll();
+
+            // config
+            //var createModel = new CreateTrainConfiguration()
+            //{
+            //    Name = "test1",
+            //    Description = "fadfa",
+            //    Width = 10,
+            //    Height = 10,
+            //    BatchSize = 10,
+            //    PretrainModelPath = "afsaf",
+            //    MaxIteration = 5,
+            //    StepCount = 5,
+            //    BaseLearningRate = 0.1,
+            //    Gamma = 0.1,
+            //    UseCacheMemory = false,
+            //    GPUIndex = 0,
+            //    SaveBestPosition = false,
+            //    SavingPercentage = 0.1
+            //};
+            //var createRes = projectClient.TrainConfigurations.Create(createModel);
+            //var createModel2 = new CreateTrainConfiguration()
+            //{
+            //    Name = "test2",
+            //    Description = "qewqewq",
+            //    Width = 10,
+            //    Height = 10,
+            //    BatchSize = 10,
+            //    PretrainModelPath = "wqeq",
+            //    MaxIteration = 5,
+            //    StepCount = 5,
+            //    BaseLearningRate = 0.1,
+            //    Gamma = 0.1,
+            //    UseCacheMemory = false,
+            //    GPUIndex = 0,
+            //    SaveBestPosition = false,
+            //    SavingPercentage = 0.1
+            //};
+            //var createRes2 = projectClient.TrainConfigurations.Create(createModel2);
+            //var getallRes = projectClient.TrainConfigurations.GetAll();
+            //var getRes = projectClient.TrainConfigurations.Get(createRes2.Id);
+            //var delRes = projectClient.TrainConfigurations.Delete(createRes2.Id);
+            //var getallRes2 = projectClient.TrainConfigurations.GetAll();
+
+            // aug
+            //var createModel = new CreateAugmentation()
+            //{
+            //    Name = "test1",
+            //    Description = "fadfa",
+            //    Mirror = true,
+            //    Flip = false,
+            //    Rotation90 = true,
+            //    Zoom = 0.1,
+            //    Shift = 0.1,
+            //    Tilt = 0.1,
+            //    Rotation = 0.1,
+            //    BorderMode = "Replicate",
+            //    Contrast = 0.1,
+            //    Brightness = 0.1,
+            //    Shade = 0.1,
+            //    Hue = 0.1,
+            //    Saturation = 0.1,
+            //    Noise = 0.1,
+            //    Smoothing = 0.1,
+            //    ColorNoise = 0.1,
+            //    PartialFocus = 0.1,
+            //    Probability = 0.1,
+            //    RandomCount = 1
+            //};
+            //var createRes = projectClient.Augmentations.Create(createModel);
+            //var createModel2 = new CreateAugmentation()
+            //{
+            //    Name = "test2",
+            //    Description = "fadfa",
+            //    Mirror = true,
+            //    Flip = false,
+            //    Rotation90 = true,
+            //    Zoom = 0.1,
+            //    Shift = 0.1,
+            //    Tilt = 0.1,
+            //    Rotation = 0.1,
+            //    BorderMode = "Replicate",
+            //    Contrast = 0.1,
+            //    Brightness = 0.1,
+            //    Shade = 0.1,
+            //    Hue = 0.1,
+            //    Saturation = 0.1,
+            //    Noise = 0.1,
+            //    Smoothing = 0.1,
+            //    ColorNoise = 0.1,
+            //    PartialFocus = 0.1,
+            //    Probability = 0.1,
+            //    RandomCount = 1
+            //};
+            //var createRes2 = projectClient.Augmentations.Create(createModel2);
+            //var getallRes = projectClient.Augmentations.GetAll();
+            //var getRes = projectClient.Augmentations.Get(createRes2.Id);
+            //var delRes = projectClient.Augmentations.Delete(createRes2.Id);
+            //var getallRes2 = projectClient.Augmentations.GetAll();
 
         }
     }
