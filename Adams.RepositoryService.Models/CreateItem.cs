27f0411833be95fdbf269b2584053bb1230be05e
deanxx <deanxx999@gmail.com>
2021-11-29 16:04:47 +0900

    add createItem required annotation

--- Adams.RepositoryService.Models/CreateItem.cs
+++ Adams.RepositoryService.Models/CreateItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Adams.RepositoryService.Models
 {
     public class CreateItem
     {
+        [Required]
         public string Tag { get; set; }
 
         public CreateItem(string tag)
