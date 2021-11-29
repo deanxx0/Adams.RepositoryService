116fcb5d4294c968c69346b7e8ec2982dc2e39ad
deanxx <deanxx999@gmail.com>
2021-11-29 15:42:49 +0900

    add item controller

---
+++ Adams.RepositoryService.Models/CreateItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adams.RepositoryService.Models
+{
+    public class CreateItem
+    {
+        public string Tag { get; set; }
+
+        public CreateItem(string tag)
+        {
+            Tag = tag;
+        }
+
+        public CreateItem()
+        {
+
+        }
+    }
+}
---
+++ Adams.RepositoryService/Controllers/ItemController.cs
@@ -0,0 +1,77 @@
+﻿using Adams.RepositoryService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NAVIAIServices.RepositoryService;
+using NAVIAIServices.RepositoryService.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adams.RepositoryService.Server.Controllers
+{
+    [ApiController]
+    [Route("")]
+    [Authorize(Policy = PolicyNames.MemberOrAdmin)]
+    public class ItemController : ControllerBase
+    {
+        IRepositoryService _repositoryService;
+        private string _projectDbRoot;
+
+        public ItemController(IRepositoryService repositoryService, IConfiguration configuration)
+        {
+            _projectDbRoot = configuration.GetValue<string>("ProjectDbRoot");
+            _repositoryService = repositoryService;
+        }
+
+        [HttpPost("projects/{projectId}/items")]
+        public ActionResult CreateItem(string projectId, [FromBody] CreateItem createItem)
+        {
+            var entity = new Item(createItem.Tag);
+            var dbPath = ConfigurationPath.Combine(_projectDbRoot, projectId + ".db");
+            if (!System.IO.File.Exists(dbPath)) return BadRequest($"Not valid projectId {projectId}");
+            var projectService = _repositoryService.GetProjectService(dbPath, DBType.LiteDB);
+            projectService.Items.Add(entity);
+            return Ok(entity);
+        }
+
+        [HttpGet("projects/{projectId}/items")]
+        public ActionResult GetAllItem(string projectId)
+        {
+            var dbPath = System.IO.Path.Combine(_projectDbRoot, projectId + ".db");
+            if (!System.IO.File.Exists(dbPath)) return BadRequest($"Not valid projectId {projectId}");
+            var projectService = _repositoryService.GetProjectService(dbPath, DBType.LiteDB);
+            var items = projectService.Items.Find(x => x.IsEnabled == true).ToList();
+            return Ok(items);
+        }
+
+        [HttpGet("projects/{projectId}/items/{itemId}")]
+        public ActionResult GetItem(string projectId, string itemId)
+        {
+            var dbPath = Path.Combine(_projectDbRoot, projectId + ".db");
+            if (!System.IO.File.Exists(dbPath)) return BadRequest($"Not valid projectId {projectId}");
+            var projectService = _repositoryService.GetProjectService(dbPath, DBType.LiteDB);
+            var item = projectService.Items.Find(x => x.IsEnabled == true && x.Id == itemId).FirstOrDefault();
+            if (item == null) return BadRequest($"Not valid itemId {itemId}");
+            return Ok(item);
+        }
+
+        [HttpDelete("projects/{projectId}/items/{itemId}")]
+        public ActionResult DeleteItem(string projectId, string itemId)
+        {
+            var dbPath = System.IO.Path.Combine(_projectDbRoot, projectId + ".db");
+            if (!System.IO.File.Exists(dbPath)) return BadRequest($"Not valid projectId {projectId}");
+            var projectService = _repositoryService.GetProjectService(dbPath, DBType.LiteDB);
+
+            var item = projectService.Items.Find(x => x.Id == itemId).FirstOrDefault();
+            if (item == null) return BadRequest($"Not valid itemId {itemId}");
+
+            item.SetValue("isenabled", false);
+
+            projectService.Items.Update(item);
+            return Ok(item);
+        }
+    }
+}
