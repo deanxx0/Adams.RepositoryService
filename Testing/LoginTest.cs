58da2175d6dc587572c51c1eaeda9a1afd7c469f
deanxx <deanxx999@gmail.com>
2021-12-02 12:37:16 +0900

    add xUnit testing

---
+++ Testing/LoginTest.cs
@@ -0,0 +1,42 @@
+﻿using Adams.RepositoryService.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Testing
+{
+    public class LoginTest
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly IAdamsClient _adamsClient;
+
+        public LoginTest(ITestOutputHelper output)
+        {
+            _output = output;
+            _adamsClient = AdamsClientFactory.Create("http://10.10.1.112:5005");
+        }
+
+        [Fact]
+        public void LoginSuccess()
+        {
+            var res = _adamsClient.LoginAsync("u1", "123").Result;
+            Assert.True(res);
+        }
+
+        [Theory]
+        [InlineData("u1", "111")]
+        [InlineData("uuu", "123")]
+        [InlineData("uu", "111")]
+        public void LoginFail(string username, string password)
+        {
+            var res = _adamsClient.LoginAsync(username, password).Result;
+            Assert.False(res);
+        }
+    }
+}
---
+++ Testing/ProjectTest.cs
@@ -0,0 +1,28 @@
+﻿using Adams.RepositoryService.Client;
+using Adams.RepositoryService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Testing
+{
+    public class ProjectTest
+    {
+        private readonly IAdamsClient _adamsClient;
+        public ProjectTest()
+        {
+            _adamsClient = AdamsClientFactory.Create("http://10.10.1.112:5005");
+        }
+
+        [Fact]
+        public void CreateProject()
+        {
+            var model = new CreateProject("testp1", "dddd", "mercury");
+            var res = _adamsClient.Projects.Create(model);
+            //Assert.Equal()
+        }
+    }
+}
